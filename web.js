aefc3184496050448fd0c37c86a18e1cd178acb9
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-01-07 15:28:30 +0100

    Provide email and download generation

--- web.js
+++ web.js
@@ -4,21 +4,25 @@
 var fs = require('fs');
 var yaml = require('js-yaml');
 var EUCopyrightQuestions = yaml.safeLoad(fs.readFileSync('_data/questions.yml', 'utf8'));
-var express = require('express');
-var app = express();
+var AdmZip = require('adm-zip');
 var postmark = require('postmark')(process.env.POSTMARK_API_KEY);
 var odtprocessor = require('./js/odtprocessor.js');
+
+var express = require('express');
+var app = express();
 
 
 var files = {
   content: fs.readFileSync('data/content.xml', 'utf8'),
-  manifest: fs.readFileSync('data/META-INF/manifest.xml', 'utf8'),
-  meta: fs.readFileSync('data/meta.xml', 'utf8'),
-  mimetype: fs.readFileSync('data/mimetype', 'utf8'),
-  settings: fs.readFileSync('data/settings.xml', 'utf8'),
-  styles: fs.readFileSync('data/styles.xml', 'utf8'),
+  manifest: fs.readFileSync('data/META-INF/manifest.xml'),
+  meta: fs.readFileSync('data/meta.xml'),
+  mimetype: fs.readFileSync('data/mimetype'),
+  settings: fs.readFileSync('data/settings.xml'),
+  styles: fs.readFileSync('data/styles.xml'),
 };
 
+var ODTContentType = 'application/vnd.oasis.opendocument.text';
+var ODTFilename = 'consultation-document_en.odt';
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', process.env.ALLOWED_DOMAIN);
@@ -32,40 +36,57 @@ app.configure(function() {
     app.use(allowCrossDomain);
 });
 
-
-var createText = function() {
-
-
-      // text = odtprocessor.renderText(text,
-      //   data
-      //   EUCopyright.questions,
-      //   EUCopyright.settings
-      // );
-
+var validateEmail = function(email) {
+  return (/\S+@\S+\.\S+/).test(email);
 };
 
-var sendEmail = function(email, data, clb){
-    postmark.send({
-        'From': process.env.FROM_EMAIL_ADDRESS,
-        'To': email,
-        'Subject': 'Test',
-        'TextBody': 'Test Message',
-        'Attachments': [{
-          'Content': '',
-          'Name': 'consultation-document_en.odt',
-          'ContentType': 'application/vnd.oasis.opendocument.text'
-        }]
-    }, function(error) {
-      clb(error);
-    });
+var createODT = function(content) {
+  var zip = new AdmZip();
+  zip.addFile('mimetype', files.mimetype);
+  zip.addFile('META-INF/manifest.xml', files.manifest);
+  zip.addFile('meta.xml', files.meta);
+  zip.addFile('settings.xml', files.settings);
+  zip.addFile('styles.xml', files.styles);
+  zip.addFile('content.xml', new Buffer(content));
+  return zip.toBuffer();
 };
 
-app.post('/mail', function(req, res){
-  var body = '{"ok": true}';
-  console.log(EUCopyrightQuestions);
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', Buffer.byteLength(body));
-  res.end(body);
+var createText = function(data, settings) {
+  return odtprocessor.renderText(
+    files.content,
+    data,
+    EUCopyrightQuestions,
+    settings
+  );
+};
+
+var sendEmail = function(email, buffer){
+  postmark.send({
+      'From': process.env.FROM_EMAIL_ADDRESS,
+      'To': email,
+      'Subject': 'Test',
+      'TextBody': 'Test Message',
+      'Attachments': [{
+        'Content': buffer.toString('base64'),
+        'Name': ODTFilename,
+        'ContentType': ODTContentType
+      }]
+  });
+};
+
+app.post('/document', function(req, res){
+  var buffer = createODT(createText(req.body, {}));
+  if (req.body.email && validateEmail(req.body.email)) {
+    console.log('Sending email to ' + req.body.email);
+    sendEmail(req.body.email, buffer);
+  }
+  if (req.query.redirect) {
+    return res.redirect(req.query.redirect);
+  }
+  res.setHeader('Content-Type', ODTContentType);
+  res.setHeader('Content-Length', buffer.length);
+  res.setHeader('Content-Disposition', 'attachment; filename="' + ODTFilename +'"');
+  res.end(buffer);
 });
 
 var port = process.env.PORT || 5000;
